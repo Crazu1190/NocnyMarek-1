ab0e3c63b94aed9dea5452bf34b3d78f0cb52252
HarrtyXX <54469508+HarrtyXX@users.noreply.github.com>
2019-09-02 11:26:58 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+  });
+  
+  client.on('message', msg => {
+    if (msg.content === '!mem') {
+      msg.channel.send('```Konkurs na mem miesiąca się jeszcze nie skończył! Konkurs skończy się dnia  10.09.2019r```');
+    }
+
+
+    if (msg.content === 'https://discord.gg/') { 
+      if(msg.member.hasPermission('administrator')) return;
+      
+      else{
+      msg.author.sendMessage("Zgodnie z regulaminem zakazuję się reklamowania serwerów poza kanałami <#610974847165661200>, <#610974781055172608> i <#610974847165661200>  po uzgodnieniu tego z <@&604841898724556860>");
+      msg.delete(10);
+      }
+    }
+    
+
+
+  });
+  
+
+client.login(config.token);
